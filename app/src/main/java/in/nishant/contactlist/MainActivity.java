ccb1267617af345921a4f1d4c5107fd14b7eae07
nishant <nishant15066@iiitd.ac.in>
2018-03-30 21:45:59 +0530

    Linked with Database successfully.

--- app/src/main/java/in/nishant/contactlist/MainActivity.java
+++ app/src/main/java/in/nishant/contactlist/MainActivity.java
@@ -2,11 +2,17 @@ package in.nishant.contactlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
-//    private DatabaseReference mDatabase, mTest;
+    DatabaseReference mDatabase, mTest;
     private final String TAG = "tag";
     private Button submit;
 
@@ -14,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mTest = mDatabase.child("Users");
+        setContentView(R.layout.activity_main);
 
+        submit = findViewById(R.id.button);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = "User 1";
+                User u = new User( "Nishant", "1", "n@sinha" );
+                mDatabase.child(name).setValue(u);
+            }
+        });
     }
 }
---
+++ app/src/main/java/in/nishant/contactlist/User.java
@@ -0,0 +1,14 @@
+package in.nishant.contactlist;
+
+public class User {
+    public String name, phone, email;
+    User()
+    {}
+
+    User( String name, String phone, String email)
+    {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
